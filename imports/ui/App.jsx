8f8d863622b7c7a3eeb52ae13f02119bbd0d7323
choisuyang <chltndid724@gmail.com>
2020-01-24 22:00:33 +0900

    toy_project_setting_complete

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Header from '../ui/HeadBar/Header.jsx'
+
+
+const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Header/>
+  </div>
+);
+
+export default App;
