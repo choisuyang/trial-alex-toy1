1d3aa498e79f9acad155b9c2361c0937a4f38046
choisuyang <chltndid724@gmail.com>
2020-02-04 20:36:28 +0900

    Make-UserInfo

---
+++ imports/ui/component/UserInfo/UserInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserInfo.scss';
+
+function UserInfo() {
+  return (
+    <div className="userInfoContainer">
+      <div className="userSearch">UserINfo Component</div>
+    </div>
+  );
+}
+
+export default UserInfo;
