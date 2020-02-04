395d4d32d2ea9416af751c57a5f128853c54d18a
choisuyang <chltndid724@gmail.com>
2020-02-04 20:34:55 +0900

    Make-SignUp-SignIn-Page

--- imports/ui/Pages/SignInPage/SignInPage.jsx
+++ imports/ui/Pages/SignInPage/SignInPage.jsx
@@ -1,49 +1,98 @@
-import React from 'react';
-import { Grid, Header, Form, Input, Segment } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Grid, Header, Form, Input, Segment, Button } from 'semantic-ui-react';
 import './SignInPage.scss';
+import { withRouter, Link } from 'react-router-dom';
 
-function SignInPage() {
+function SignInPage({ history }) {
+  const [loginId, setLoginId] = useState('');
+  const [password, setPassword] = useState('');
+
+  // console.log('History', history);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('I am ID', loginId);
+    console.log('I am PWD', password);
+
+    Meteor.loginWithPassword(loginId, password, err => {
+      if (err) {
+        alert('실패');
+        console.log('로그인 실패');
+      } else {
+        alert('성공');
+        console.log('로그인 성공');
+        history.push('/');
+      }
+    });
+  };
+
+  const onChangeId = e => {
+    setLoginId(e.target.value);
+    console.log('id', loginId);
+  };
+
+  const onChangePwd = e => {
+    setPassword(e.target.value);
+    console.log('pwd', password);
+  };
+
   return (
-    <Grid size="huge" centered columns={2}>
-      <Grid.Row>
-        <Grid.Column>
+    <div>
+      <Grid size="huge" centered columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <br />
+            <Header color="teal" as="h1" textAlign="center">
+              Log In
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Segment>
+          <Grid.Row>
+            <Grid.Column>
+              <Form.Field inline>
+                <label>Name or Email</label>
+                <Input
+                  size="small"
+                  type="text"
+                  icon="user"
+                  placeholder="error@mail.com"
+                  onChange={onChangeId}
+                />
+              </Form.Field>
+            </Grid.Column>
+          </Grid.Row>
           <br />
-          <Header color="teal" as="h1" textAlign="center">
-            Log In
-          </Header>
-        </Grid.Column>
-      </Grid.Row>
-      <Segment>
-        <Grid.Row>
-          <Grid.Column>
-            <Form.Field inline>
-              <label>Name or Email</label>
-              <Input
-                size="small"
-                type="text"
-                icon="user"
-                placeholder="error@mail.com"
-              />
-            </Form.Field>
-          </Grid.Column>
-        </Grid.Row>
-        <br />
-        <Grid.Row>
-          <Grid.Column>
-            <Form.Field inline>
-              <label>Password </label>
-              <Input
-                size="small"
-                type="password"
-                icon="lock"
-                placeholder="Password"
-              />
-            </Form.Field>
-          </Grid.Column>
-        </Grid.Row>
-      </Segment>
-    </Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Form.Field inline>
+                <label>Password </label>
+                <Input
+                  size="small"
+                  type="password"
+                  icon="lock"
+                  placeholder="Password"
+                  onChange={onChangePwd}
+                />
+              </Form.Field>
+            </Grid.Column>
+          </Grid.Row>
+        </Segment>
+      </Grid>
+      <div className="buttonContainer">
+        <Link to="/">
+          <Button inverted color="blue">
+            Cancel
+          </Button>
+        </Link>
+
+        <Button inverted color="red" type="submit" onClick={onSubmit}>
+          OK
+        </Button>
+      </div>
+    </div>
   );
 }
 
-export default SignInPage;
+export default withRouter(SignInPage);
--- imports/ui/Pages/SignUp/SignUpPage.jsx
+++ imports/ui/Pages/SignUp/SignUpPage.jsx
@@ -8,14 +8,17 @@ import {
   Image,
   Input,
   Label,
+  BreadcrumbSection,
 } from 'semantic-ui-react';
 import './SignUpPage.scss';
 
-import { isEmail, isValidation } from './Auth.jsx.js';
+import { isEmail, isValidation } from '../SignUp/Auth.jsx';
+import { Meteor } from 'meteor/meteor';
+// import { Accounts } from 'meteor/accounts-base';
 
 // NOTE Main Page
 
-function SignUpPage() {
+function SignUpPage({ history }) {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
   const [name, setName] = useState('');
@@ -25,35 +28,31 @@ function SignUpPage() {
   const [passwordError, setPasswordError] = useState(false);
   const [boolean, setBoolean] = useState(true);
 
-  const isValidInputEmail = email => {
-    return;
-  };
-
   const onChangeEmail = e => {
     setEmail(e.target.value);
-    console.log('Target Email : ', e.target.value);
+    // console.log('Target Email : ', e.target.value);
   };
 
   const onChangeName = e => {
     setName(e.target.value);
-    console.log('Target Name : ', e.target.value);
+    // console.log('Target Name : ', e.target.value);
   };
 
   const onChangePassword = e => {
     setPassword(e.target.value);
-    console.log('Target Password :', e.target.value);
+    // console.log('Target Password :', e.target.value);
   };
 
   const onChangeCheckPassword = e => {
     setPasswordError(e.target.value !== password);
     setCheckPassword(e.target.value);
 
-    console.log('Target RePassword :', e.target.value);
+    // console.log('Target RePassword :', e.target.value);
   };
 
   const onChangePhoneNumber = e => {
     setPhoneNumber(e.target.value);
-    console.log('Target PhoneNumber :', e.target.value);
+    // console.log('Target PhoneNumber :', e.target.value);
   };
 
   const onTextValidation = () => {
@@ -69,11 +68,42 @@ function SignUpPage() {
     const valid = onTextValidation();
     setBoolean(valid);
 
-    console.log('Email Validation', valid);
+    // console.log('Email Validation', valid);
     if (!valid) {
       console.log('Fail');
     }
+
+    const inputUser = {
+      email,
+      name,
+      password,
+      checkPassword,
+      phoneNumber,
+    };
+
+    Meteor.call('signup', inputUser, err => {
+      if (err) {
+        console.log('실패');
+        alert('아이디가 이미 있거나 회원이 아닙니다.');
+      } else {
+        console.log('성공');
+        alert('환영합니다.');
+        history.push('/signin');
+      }
+    });
   };
+
+  const buttonValidation =
+    email.length > 3 &&
+    name.length > 1 &&
+    password.length > 1 &&
+    phoneNumber.length > 9;
+
+  console.log('Check button', buttonValidation);
+
+  console.log('check', email.length);
+  console.log('name', name.length);
+  console.log('sum', email.length && name.length ? 'true' : 'false');
 
   return (
     <Grid centered columns={2}>
@@ -142,9 +172,9 @@ function SignUpPage() {
               name="name"
               value={name}
             />
-            {/* <Label basic color="red" pointing="left">
+            <Label basic color="red" pointing="left">
               'Please enter a Name'
-            </Label> */}
+            </Label>
           </Form.Field>
           <Form.Field inline>
             <label>Password</label>
@@ -173,14 +203,15 @@ function SignUpPage() {
               value={checkPassword}
             />
             <Label basic color="red" pointing="left">
-              {passwordError ? 'Please enter a Password' : 'Correct'}
+              {!checkPassword.length || passwordError
+                ? 'Please enter a Password'
+                : 'Correct'}
             </Label>
           </Form.Field>
           <Form.Field inline>
             <label>Phone Number</label>
             <Input
               size="small"
-              type="Number"
               icon="phone"
               placeholder="Phone Number"
               onChange={onChangePhoneNumber}
@@ -190,23 +221,27 @@ function SignUpPage() {
             <Label basic color="red" pointing="left">
               'Please enter a Phone Number'
             </Label>
-
-            <Grid.Row columns={3}>
-              <br />
-              <Button size="small" color="grey">
-                Cancel
-              </Button>
-              <Button
-                type="submit"
-                onClick={onSubmit}
-                size="small"
-                color="blue"
-              >
-                OK
-              </Button>
-            </Grid.Row>
           </Form.Field>
         </Form>
+        <Grid.Row columns={3}>
+          <br />
+          <Button
+            onClick={() => console.log('Cancel')}
+            size="small"
+            color="grey"
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={!buttonValidation}
+            type="submit"
+            onClick={onSubmit}
+            size="small"
+            color="blue"
+          >
+            OK
+          </Button>
+        </Grid.Row>
       </Segment>
 
       <Grid.Row>
