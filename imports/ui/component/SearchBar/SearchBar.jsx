a66b432affcb58a1226be773baffcc2674ed5c07
choisuyang <chltndid724@gmail.com>
2020-02-10 21:14:23 +0900

    SearchBar 수정중

--- imports/ui/component/SearchBar/SearchBar.jsx
+++ imports/ui/component/SearchBar/SearchBar.jsx
@@ -13,7 +13,14 @@ import {
 import { withTracker } from 'meteor/react-meteor-data';
 import './SearchItem.scss';
 
-function SearchBar({ users }) {
+const SearchBar = props => {
+  const { users } = props;
+  const testUsers = [];
+  _.times(80, () => {
+    _.each(users, u => {
+      testUsers.push(u);
+    });
+  });
   const userData = _.map(users, (state, index) => ({
     key: state._id,
     text: state.username,
@@ -35,50 +42,33 @@ function SearchBar({ users }) {
 
   return (
     <Segment>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={16}>
-            <Dropdown
-              fluid
-              multiple
-              onChange={onChangeHandler}
-              onSearchChange={onSearchChangeHandler}
-              options={userData}
-              placeholder="검색하세요"
-              searchQuery={searchQuery}
-              selection
-              search
-              value={searchQuery}
-            />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={16}>
-            <div className="searchItem">
-              {_.map(users, (item, index) => (
-                <List animated verticalAlign="middle" key={index}>
-                  <List.Item>
-                    <Image
-                      avatar
-                      src="https://react.semantic-ui.com/images/avatar/small/helen.jpg"
-                    />
-                    <List.Content>
-                      <List.Header>
-                        {searchQuery === item.username
-                          ? searchQuery
-                          : item.username}
-                      </List.Header>
-                    </List.Content>
-                  </List.Item>
-                </List>
-              ))}
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <div>Search</div>
+      <div>
+        List Container
+        <div className="searchItem">
+          {_.map(testUsers, (item, index) => (
+            <List animated verticalAlign="middle" key={index}>
+              <List.Item>
+                <Image
+                  avatar
+                  src="https://react.semantic-ui.com/images/avatar/small/helen.jpg"
+                />
+                <List.Content>
+                  <List.Header>
+                    {searchQuery === item.username
+                      ? searchQuery
+                      : item.username}
+                  </List.Header>
+                </List.Content>
+              </List.Item>
+            </List>
+          ))}
+        </div>
+      </div>
     </Segment>
   );
-}
+};
+
 export default withTracker(() => {
   Meteor.subscribe('users');
   return {
@@ -86,3 +76,63 @@ export default withTracker(() => {
     currentUser: Meteor.user(),
   };
 })(SearchBar);
+
+// const BKUP__OLD = (
+//   <Grid>
+//     <Grid.Row>
+//       <Grid.Column width={16}>
+//         <Dropdown
+//           fluid
+//           multiple
+//           onChange={onChangeHandler}
+//           onSearchChange={onSearchChangeHandler}
+//           options={userData}
+//           placeholder="검색하세요"
+//           searchQuery={searchQuery}
+//           selection
+//           search
+//           value={searchQuery}
+//         />
+//       </Grid.Column>
+//     </Grid.Row>
+//     <Grid.Row>
+//       <Grid.Column width={16}>
+//         <div
+//           style={{
+//             display: 'flex',
+//             backgroundColor: 'red',
+//             height: '100%',
+//           }}
+//         >
+//           <div
+//             style={{
+//               backgroundColor: 'green',
+//               alignSelf: 'stretch',
+//             }}
+//           >
+//             hi
+//           </div>
+//         </div>
+//         {/* <div className="searchItem">
+//         {_.map(testUsers, (item, index) => (
+//           <List animated verticalAlign="middle" key={index}>
+//             <List.Item>
+//               <Image
+//                 avatar
+//                 src="https://react.semantic-ui.com/images/avatar/small/helen.jpg"
+//               />
+//               <List.Content>
+//                 <List.Header>
+//                   {searchQuery === item.username
+//                     ? searchQuery
+//                     : item.username}
+//                 </List.Header>
+//               </List.Content>
+//             </List.Item>
+//           </List>
+//         ))}
+//       </div> */}
+//       </Grid.Column>
+//     </Grid.Row>
+//   </Grid>
+// );
